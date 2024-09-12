6d4668a3391b76a0125b4bda7aac6abf9586485e
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2024-09-12 16:14:00 +0200

    Tattoos Page v1.1 (ajout d'un filtre de recherche par titre)

--- app/Http/Controllers/TattooController.php
+++ app/Http/Controllers/TattooController.php
@@ -3,17 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use App\Models\Portfolio;
 
 class TattooController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Récupérer toutes les images de tatouages des portfolios des tatoueurs
-        $tattooImages = Portfolio::inRandomOrder()->get();
+        // Obtenir le texte de recherche de l'utilisateur
+        $search = $request->input('search');
 
-        // Retourner la vue avec les images de tatouages
+        // Récupérer les images de tatouage des portfolios avec filtre de recherche
+        $tattooImages = Portfolio::with('user')
+            ->when($search, function ($query, $search) {
+                $query->where('title', 'LIKE', "%{$search}%");
+            })
+            ->inRandomOrder()
+            ->get();
+
         return view('tattoos.index', compact('tattooImages'));
     }
 }
+
