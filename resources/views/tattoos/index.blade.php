9d21d967d0a6809b9d17a5696681ba9ca87af5b5
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2024-09-12 16:14:31 +0200

    Tattoos Page v1.1 (ajout d'un filtre par titre)

--- resources/views/tattoos/index.blade.php
+++ resources/views/tattoos/index.blade.php
@@ -6,13 +6,22 @@
 <div class="tattoos-container">
     <h1>Découvrir des Tatouages</h1>
 
+    <!-- Champ de recherche -->
+    <div class="search-bar">
+        <form action="{{ route('tattoos.index') }}" method="GET">
+            <input type="text" name="search" placeholder="Rechercher par titre de tatouage..." value="{{ request('search') }}">
+            <button type="submit">Rechercher</button>
+        </form>
+    </div>
+
     <!-- Fil d'actualité de tatouages -->
     <div class="tattoo-feed">
         @foreach ($tattooImages as $tattoo)
             <div class="tattoo-item">
                 <img src="{{ asset('storage/' . $tattoo->image_path) }}" alt="Tattoo by {{ $tattoo->user->name }}">
                 <div class="tattoo-info">
-                    <p>&#64;{{ $tattoo->user->login }}</p>
+                    <p><strong>{{ $tattoo->title }}</strong></p>
+                    <p><a href="{{ route('profile.showProfile', $tattoo->user->login) }}">&#64;{{ $tattoo->user->login }}</a></p>
                 </div>
             </div>
         @endforeach
